fafd0540fe2a33bac0c1fc7703f9b6fbea82bf7f
owen <owen@premierqfutures.com>
2021-02-23 09:51:07 +0800

    f

--- src/components/common/dialog/index.tsx
+++ src/components/common/dialog/index.tsx
@@ -1,7 +1,36 @@
-import React from 'react'
-
-function Dialog() {
-  return <div>Dialog</div>
+import React, { useEffect, memo, ReactChild } from 'react'
+import ReactDOM from 'react-dom'
+import Style from './index.module.css'
+interface DialogType {
+  visible: boolean
+  closeDialog: Function
+  children: ReactChild
 }
 
-export default Dialog
+function Dialog({ visible, closeDialog, children }: DialogType) {
+  useEffect(() => {
+    console.log('Dialog')
+  })
+
+  function handleCloseDialog() {
+    closeDialog()
+  }
+
+  return visible
+  ? <div className={Style['dialog-wrap']} onClick={handleCloseDialog}>{ children }</div>
+  : null
+}
+
+function areEqual(prevProps: DialogType, nextProps: DialogType) {
+  /*
+  如果把 nextProps 传入 render 方法的返回结果与
+  将 prevProps 传入 render 方法的返回结果一致则返回 true，
+  否则返回 false
+  */
+ return prevProps.visible === nextProps.visible
+}
+
+export default memo(
+  Dialog,
+  areEqual
+)
--- src/views/public-module/home/index.tsx
+++ src/views/public-module/home/index.tsx
@@ -1,5 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Dialog from 'components/common/dialog'
 
 export default function Home() {
-  return <h1>home</h1>
+  const [visible, setVisible] = useState(false)
+  const [num, setNum] = useState(0)
+
+  function handleDialogVisible() {
+    setVisible(!visible)
+  }
+
+  return <>
+    <h1>home</h1>
+    <Dialog visible={visible} closeDialog={handleDialogVisible} ><h2>dialog</h2></Dialog>
+    <button onClick={handleDialogVisible}>handleDialogVisible</button>
+    <br />
+    {num}
+    <button onClick={
+      () => {
+        setNum(num + 1)
+      }
+    }>add</button>
+  </>
 }
