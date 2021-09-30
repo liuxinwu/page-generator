079a60e99bcefbe3651a64277f6bc18531796f91
xinwu.liu <xinwu.liu@corp.to8to.com>
2021-09-30 14:50:53 +0800

    feat: 代码优化

--- src/components/common/operate/index.tsx
+++ src/components/common/operate/index.tsx
@@ -1,166 +1,142 @@
 import { useState, useEffect, useRef, useCallback } from 'react'
 import Style from './index.module.css'
 import useMouseEvent from 'hooks/useMouseEvent'
-import { connect } from 'react-redux'
-import {
-  StateType,
-  UseComponentsType,
-  ActionType,
-  ActiveComponentType,
-} from 'store/type'
+import { useDispatch } from 'react-redux'
 import classnames from 'classnames'
 
-const mapState = (state: StateType) => ({
-  useComponents: state.useComponents,
-})
-const mapDispatch = (dispatch: any) => ({
-  changeUseComponents: (val: ActionType<UseComponentsType>) => dispatch(val),
-  replaceActiveComponent: (value: ActiveComponentType) => {
-    dispatch({
-      type: 'REPLACE_ACTIVE_COMPONENTS',
-      value,
-    })
-  },
-})
 interface ParentType extends HTMLDivElement {
   isRoot?: boolean | undefined
   name?: string
 }
 
+// 活动的 dom
 const activityDomMap = new Map<string, Function>()
 
 /**
  * 1. 绑定双击事件
  * 2. 添加一些操作功能（拖拽改大小、拖拽改位置、取消操作）
  */
-const Operate = connect(
-  mapState,
-  mapDispatch
-)(
-  React.memo(
-    function ({
-      children,
-      currentEl: parent,
-      name,
-      changeUseComponents,
-      replaceActiveComponent,
-      ...props
-    }: React.PropsWithChildren<{
-      name: string
-      children?: React.ReactNode
-      currentEl?: ParentType | string
-      changeUseComponents: (val: ActionType<UseComponentsType>) => void
-      replaceActiveComponent: (val: ActiveComponentType) => void
-    }>) {
-      // 当前操作的元素
-      const el = useRef<ParentType>()
-      const { moveOffset, handleMouseDown } = useMouseEvent()
+const Operate = React.memo(
+  function ({
+    children,
+    currentEl: parent,
+    name,
+  }: React.PropsWithChildren<{
+    name: string
+    children?: React.ReactNode
+    currentEl?: ParentType | string
+  }>) {
+    // 当前操作的元素
+    const el = useRef<ParentType>()
+    const { moveOffset, handleMouseDown } = useMouseEvent()
+    const dispatch = useDispatch()
 
-      // 是否显示操作功能组件
-      const [visible, setVisible] = useState(false)
+    // 是否显示操作功能组件
+    const [visible, setVisible] = useState(false)
 
-      // 元素聚焦事件
-      const handleFocus = useCallback(
-        (e: MouseEvent) => {
-          const currentEl = el.current
-          setVisible(true)
-          e.stopPropagation()
+    // 元素聚焦事件
+    const handleFocus = useCallback(
+      (e: MouseEvent) => {
+        const currentEl = el.current
+        setVisible(true)
+        e.stopPropagation()
 
-          replaceActiveComponent({ name, dom: currentEl })
-          // 去除其它元素的聚焦状态
-          activityDomMap.forEach((blur, _name) => name !== _name && blur())
-        },
-        [name, replaceActiveComponent]
-      )
+        name !== 'root' && dispatch({
+          type: 'REPLACE_ACTIVE_COMPONENTS',
+          value: { name, dom: currentEl }
+        })
+        // 去除其它元素的聚焦状态
+        activityDomMap.forEach((blur, _name) => name !== _name && blur())
+      },
+      [name, dispatch]
+    )
 
-      // 元素失焦事件
-      function handleBlur() {
-        setVisible(false)
+    // 元素失焦事件
+    function handleBlur() {
+      setVisible(false)
+    }
+
+    // 获取元素、并标识元素类型
+    // 默认聚焦
+    // 事件监听
+    useEffect(() => {
+      if (!parent) return
+      if (typeof parent === 'string') {
+        const _currentEl = document.querySelector(`#${parent}`) as ParentType
+        // 标志是否是根元素
+        _currentEl!.isRoot = true
+        _currentEl!.name = name
+        el.current = _currentEl
+      } else {
+        parent.name = name
+        el.current = parent
       }
 
-      // 获取元素、并标识元素类型
-      // 默认聚焦
-      // 事件监听
-      useEffect(() => {
-        if (!parent) return
-        if (typeof parent === 'string') {
-          const _currentEl = document.querySelector(`#${parent}`) as ParentType
-          // 标志是否是根元素
-          _currentEl!.isRoot = true
-          _currentEl!.name = name
-          el.current = _currentEl
-        } else {
-          parent.name = name
-          el.current = parent
-        }
+      const currentEl = el.current
+      activityDomMap.set(name, handleBlur)
 
-        const currentEl = el.current
-        activityDomMap.set(name, handleBlur)
+      currentEl.addEventListener('click', handleFocus)
+      !currentEl.isRoot &&
+        currentEl.addEventListener('mousedown', handleMouseDown)
+      window.addEventListener('click', handleBlur)
 
-        currentEl.addEventListener('click', handleFocus)
+      return () => {
+        currentEl.removeEventListener('click', handleFocus)
         !currentEl.isRoot &&
-          currentEl.addEventListener('mousedown', handleMouseDown)
-        window.addEventListener('click', handleBlur)
+          currentEl.removeEventListener('mousedown', handleMouseDown)
+        window.removeEventListener('click', handleBlur)
+      }
+    }, [parent, name, handleMouseDown, handleFocus])
 
-        return () => {
-          currentEl.removeEventListener('click', handleFocus)
-          !currentEl.isRoot &&
-            currentEl.removeEventListener('mousedown', handleMouseDown)
-          window.removeEventListener('click', handleBlur)
-        }
-      }, [parent, name, handleMouseDown, handleFocus])
+    // 拖拽改变位置
+    useEffect(() => {
+      const currentEl = el.current
+      if (currentEl === undefined) return
+      if (currentEl.isRoot) return
 
-      // 拖拽改变位置
-      useEffect(() => {
-        const currentEl = el.current
-        if (currentEl === undefined) return
-        if (currentEl.isRoot) return
+      // 通过修改 translate 来改变位置
+      // 替换对 left、top 的修改、避免不必要的回流与重绘
+      const style = currentEl.style
+      const { x, y } = moveOffset
+      const [ , , tLeft = 0, tTop = 0] = style.transform?.match(/((-{0,1}\d+px), (-{0,1}\d+px))/) ?? []
+      style.transform = `translate(${parseInt(`${tLeft}`) + x}px, ${parseInt(`${tTop}`) + y}px)`
+      const cssText = currentEl.style.cssText
+      
+      dispatch({
+        type: 'EDIT_USE_COMPONENTS',
+        value: {
+          name: currentEl.name as string,
+          css: cssText,
+        },
+      })
+    }, [moveOffset, dispatch])
 
-        // 通过修改 translate 来改变位置
-        // 替换对 left、top 的修改、避免不必要的回流与重绘
-        const style = currentEl.style
-        const { x, y } = moveOffset
-        const [ , , tLeft = 0, tTop = 0] = style.transform?.match(/((-{0,1}\d+px), (-{0,1}\d+px))/) ?? []
-        style.transform = `translate(${parseInt(`${tLeft}`) + x}px, ${parseInt(`${tTop}`) + y}px)`
-        const cssText = currentEl.style.cssText
-        
-        changeUseComponents({
-          type: 'EDIT_USE_COMPONENTS',
-          value: {
-            name: currentEl.name as string,
-            css: cssText,
-          },
-        })
-      }, [moveOffset, changeUseComponents])
+    return (
+      <>
+        {children}
+        {visible && (
+          <>
+            <DragSizeIcon
+              currentEl={el.current}
+            />
+          </>
+        )}
 
-      return (
-        <>
-          {children}
-          {visible && (
-            <>
-              <DragSizeIcon
-                currentEl={el.current}
-                changeUseComponents={changeUseComponents}
-              />
-            </>
-          )}
-
-          <div
-            className={'tip_line'}
-            style={{ display: visible ? 'block' : 'none' }}
-          >
-            <span className={classnames(Style.line, Style.top_line)}></span>
-            <span className={classnames(Style.line, Style.right_line)}></span>
-            <span className={classnames(Style.line, Style.bottom_line)}></span>
-            <span className={classnames(Style.line, Style.left_line)}></span>
-          </div>
-        </>
-      )
-    },
-    (prevProps, nextProps) => {
-      return true
-    }
-  )
+        <div
+          className={'tip_line'}
+          style={{ display: visible ? 'block' : 'none' }}
+        >
+          <span className={classnames(Style.line, Style.top_line)}></span>
+          <span className={classnames(Style.line, Style.right_line)}></span>
+          <span className={classnames(Style.line, Style.bottom_line)}></span>
+          <span className={classnames(Style.line, Style.left_line)}></span>
+        </div>
+      </>
+    )
+  },
+  (prevProps, nextProps) => {
+    return true
+  }
 )
 export default Operate
 
@@ -169,13 +145,12 @@ export default Operate
  * @param param0
  */
 function DragSizeIcon({
-  currentEl,
-  changeUseComponents,
+  currentEl
 }: {
   currentEl: ParentType | undefined
-  changeUseComponents: (val: ActionType<UseComponentsType>) => void
 }) {
   const { moveOffset, handleMouseDown } = useMouseEvent()
+  const dispatch = useDispatch()
 
   useEffect(() => {
     if (currentEl === undefined) return
@@ -196,14 +171,14 @@ function DragSizeIcon({
       document.body.scrollTop = document.documentElement.scrollTop =
         scrollTop + moveOffset.y
     }
-    changeUseComponents({
+    dispatch({
       type: 'EDIT_USE_COMPONENTS',
       value: {
         name: currentEl.name as string,
         css: cssText,
       },
     })
-  }, [currentEl, moveOffset, changeUseComponents])
+  }, [currentEl, moveOffset])
 
   return (
     <i
